a7b1bc4a827256b72e12999c8e04baa14d445ace
colton lacey <coltnutmiles14@gmail.com>
2019-06-21 09:54:22 -0700

    add rout links on app component html

--- WebsiteRebuild/src/app/app.routing.ts
+++ WebsiteRebuild/src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
 import { ItemListComponent } from './item-list/item-list.component';
+import { AboutComponent }   from './about/about.component'; 
 
 
 const appRoutes: Routes = [
@@ -10,6 +11,11 @@ const appRoutes: Routes = [
   path: '',
   component: ItemListComponent
 },
+
+{
+path: 'about',
+component: AboutComponent
+}
 
 ];
 
