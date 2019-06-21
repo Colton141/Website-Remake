82d4439f499e2fbfaa1b1796a3673d1ffbba1600
colton lacey <coltnutmiles14@gmail.com>
2019-06-21 10:02:03 -0700

    add routing path for welcome componnent

--- WebsiteRebuild/src/app/app.routing.ts
+++ WebsiteRebuild/src/app/app.routing.ts
@@ -1,7 +1,8 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
 import { ItemListComponent } from './item-list/item-list.component';
-import { AboutComponent }   from './about/about.component'; 
+import { AboutComponent }   from './about/about.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 const appRoutes: Routes = [
@@ -9,12 +10,17 @@ const appRoutes: Routes = [
 
 {
   path: '',
-  component: ItemListComponent
+  component: WelcomeComponent
 },
 
 {
 path: 'about',
 component: AboutComponent
+},
+
+{
+  path: 'itemlist',
+  component: ItemListComponent
 }
 
 ];
