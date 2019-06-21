d4eece68a7a61721e7f17bdcef92945bf14f0052
colton lacey <coltnutmiles14@gmail.com>
2019-06-21 11:20:45 -0700

    add items to item list component

--- WebsiteRebuild/src/app/item-list/item-list.component.ts
+++ WebsiteRebuild/src/app/item-list/item-list.component.ts
@@ -7,6 +7,13 @@ import { Item } from '../models/Items.model';
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent {
+  masterItemList: Item[] = [
+    new Item('Item1', 'erunt mollit anim id est laborum.', 12, 'class1', '5032285667'),
+    new Item('itme2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 20, 'class2', '5038875584'),
+    new Item('item3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4, 'class1', '5039048009')
+    ];
+
+    
   @Input() childItemList: Item[];
    @Output() clickSender = new EventEmitter();
 
