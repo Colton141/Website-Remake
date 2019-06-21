080776922c0436e6cdc4867f0328fc1701853db7
colton lacey <coltnutmiles14@gmail.com>
2019-06-21 16:22:32 -0700

    remove items from item list component

--- WebsiteRebuild/src/app/item-list/item-list.component.ts
+++ WebsiteRebuild/src/app/item-list/item-list.component.ts
@@ -11,11 +11,7 @@ export class ItemListComponent {
 
   constructor(private router: Router) { }
 
-  items: Item[] = [
-    new Item('Item1', 'erunt mollit anim id est laborum.', 12, 'class1', '5032285667', 1),
-    new Item('itme2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 20, 'class2', '5038875584', 2),
-    new Item('item3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4, 'class1', '5039048009', 3)
-    ];
+  items: Item[] = [];
 
     goToDetailPage(clickedItem: Item) {
       this.router.navigate(['items', clickedItem.id])
