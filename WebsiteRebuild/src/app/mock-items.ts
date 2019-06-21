585f0a65805d95ba10416fbd88636bcd3eae5613
colton lacey <coltnutmiles14@gmail.com>
2019-06-21 15:45:58 -0700

    add mock items file

---
+++ WebsiteRebuild/src/app/mock-items.ts
@@ -0,0 +1,7 @@
+import { Item } from './item-list.model';
+
+export const ITEM: Item[] = [
+  new Item('Item1', 'erunt mollit anim id est laborum.', 12, 'class1', '5032285667', 1),
+  new Item('itme2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 20, 'class2', '5038875584', 2),
+  new Item('item3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4, 'class1', '5039048009', 3)
+  ];
