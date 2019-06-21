245715d231e65098ab46dea18f228797c041e654
colton lacey <coltnutmiles14@gmail.com>
2019-06-21 09:17:25 -0700

    add sample item json file with items

--- WebsiteRebuild/src/app/app.module.ts
+++ WebsiteRebuild/src/app/app.module.ts
@@ -2,12 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { EditItemComponent } from './edit-item/edit-item.component';
 import { NewItemComponent } from './new-item/new-item.component';
+
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 
 @NgModule({
