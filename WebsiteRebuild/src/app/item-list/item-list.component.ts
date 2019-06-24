0f5c994f4e7bd5e07f05517dabb94e5dfc2a697d
colton lacey <coltnutmiles14@gmail.com>
2019-06-23 19:58:06 -0700

    link item list component to firebase

--- WebsiteRebuild/src/app/item-list/item-list.component.ts
+++ WebsiteRebuild/src/app/item-list/item-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input,, OnInit, Output, EventEmitter } from '@angular/core';
 import { Item } from '../models/Items.model';
 import { Router } from '@angular/router';
 import { ItemService } from '../item.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-item-list',
@@ -10,6 +11,7 @@ import { ItemService } from '../item.service';
   providers: [ItemService]
 })
 export class ItemListComponent implements OnInit {
+  items: FirebaseListObservable<any[]>;
 
   constructor(private router: Router, private itemService: ItemService) { }
 
--- WebsiteRebuild/src/app/item.service.ts
+++ WebsiteRebuild/src/app/item.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Item } from './item-list.model';
 import { ITEMS } from './mock-items';
 
 @Injectable()
 export class ItemService {
+    items: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+  this.items = database.list('items');
+}
 
   getItems() {
-  return ITEMS;
+  return this.items;
   }
 
   getItemById(itemId: number){
